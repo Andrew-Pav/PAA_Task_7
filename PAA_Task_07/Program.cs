50cbd21278bcea87a469bdbb6d614f98c54ccaae
Andrew_Pav <igorjizn@mail.ru>
2020-11-29 22:45:34 +0300

    PAA_Task_07

---
+++ PAA_Task_07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PAA_Task_07
+{
+    class Program
+    {
+        private static void Shuffle(int[] arr)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var rndInd = rand.Next(0, arr.Length);
+                var rndInd2 = rand.Next(0, arr.Length);
+                var temp = arr[rndInd];
+                arr[rndInd] = arr[rndInd2];
+                arr[rndInd2] = temp;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.Write("Введите количество случайных чисел: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[num];
+            Console.Write("Массив из случайных чисел: ");
+            for (int i = 0; i < num; i++)
+            {
+                a[i] = rand.Next(100);
+                Console.Write(a[i] + " ");
+            }
+            Console.Write("\nПеремешанный массив: ");
+            Shuffle(a);
+            Console.WriteLine(string.Join(" ", a));
+        }
+    }
+}
